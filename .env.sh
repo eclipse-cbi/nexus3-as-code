209c53583922e39ffff2459947dae7c97a9799b1
sebastien.heurtematte <sebastien.heurtematte@eclipse-foundation.org>
2026-01-19 17:22:37 +0100

    feat: add start message
    Signed-off-by: sebastien.heurtematte <sebastien.heurtematte@eclipse-foundation.org>

--- .env.sh
+++ .env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 vaultctl login 
+
+echo "Exporting Vault secrets and user credentials..."
 eval $(vaultctl export-vault)
 eval $(vaultctl export-users-cbi NEXUS_USERNAME NEXUS_PASSWORD)
 export NEXUS_ENV="prod"
